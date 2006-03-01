350e52d96c964e53dcc9db871cd6cf60fffc61b7
golovizin <>
2006-03-01 15:24:10 +0000

    enhanced abbreviate function

--- utils.py
+++ utils.py
@@ -1,6 +1,28 @@
+terminators = '.?!'
+
+def is_terminator(s):
+    return (bool(s) and s in terminators)
+
 def add_period(s):
-    if not (s.endswith('.') or s.endswith('?') or s.endswith('!')):
+    if not is_terminator(s[-1]):
         return s + '.'
 
 def abbreviate(s):
-    return add_period(s[0].upper())
+    def parts(s):
+        start = 0
+        length = 0
+        for letter in s:
+            length += 1
+            if not letter.isalpha():
+                yield s[start:length], letter
+                start += length
+                length = 0
+        yield s[start:length], ""
+    def abbr(part):
+        if is_terminator(part[1]):
+            print 'in', part
+            return part[0][0].upper() + part[1]
+        else:
+            print 'not in'
+            return part[0][0].upper() + '.'
+    return ''.join(abbr(part) for part in parts(s))
