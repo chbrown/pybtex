8ba1b0d0029ac8c35b842c54856a38247a918382
golovizin <>
2006-03-03 17:26:33 +0000

    unsrt style produces BibTeX-compatible output for articles and books

--- formatters/latex/unsrt.py
+++ formatters/latex/unsrt.py
@@ -11,13 +11,17 @@ class Formatter(base.Formatter):
     def format_title(self, title):
         return utils.add_period(title)
        
-    def format_article(self, entry):
+    def format_article(self, e):
         l = []
-        l.append(self.format_authors(entry.authors))
-        l.append(self.format_title(entry['title']))
-        j = latex.emph(entry['journal'])
-        y = utils.format(entry['year'], ', %s')
-        l.append("%s%s" % (j, y))
+        l.append(self.format_authors(e.authors))
+        l.append(self.format_title(e['title']))
+        journal = latex.emph(e['journal'])
+        if e.has_key('volume'):
+            vp = "".join([', ', e['volume'], utils.format(e['pages'], ':%s')])
+        else:
+            vp = utils.format(e['pages'], ', pages %s')
+        year = utils.format(e['year'], ', %s')
+        l.append("".join([journal, vp, year]))
         return l
         
     def format_book(self, entry):
@@ -28,4 +32,3 @@ class Formatter(base.Formatter):
         y = utils.format(entry['year'], ', %s')
         l.append("%s%s" % (p, y))
         return l
-        
