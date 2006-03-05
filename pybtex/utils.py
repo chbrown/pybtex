e533c006821a50add2a61d033856b98d7980588c
golovizin <>
2006-03-05 18:23:53 +0000

    added Packer class

--- pybtex/utils.py
+++ pybtex/utils.py
@@ -30,3 +30,47 @@ def format(s, format = "%s"):
         return format % s
     else:
         return ""
+
+class Packer:
+    def __init__(self, text=None, sep=', ', sep2=None, last_sep=None, add_period=False):
+        self.parts = []
+        self.append(text)
+        self.sep = sep
+        if last_sep is None:
+            last_sep = sep
+        self.last_sep = last_sep
+        if sep2 is None:
+            sep2 = last_sep
+        self.sep2 = sep2
+        self.add_period = add_period
+        self.sep_after = None
+
+    def append(self, text, sep_before=None, sep_after=None):
+        if text is not None:
+            text = str(text)
+            if self.sep_after is not None:
+                sep_before = self.sep_after
+                self.sep_after = None
+            if sep_after is not None:
+                self.sep_after = sep_after
+            if text:
+                self.parts.append((text, sep_before))
+
+    def __str__(self):
+        def output_part(part, sep):
+            if part[1] is not None:
+                sep = part[1]
+            tmp.append(sep + part[0])
+
+        if len(self.parts) == 2:
+            text = self.sep2.join(self.parts)
+        else:
+            tmp = []
+            output_part(self.parts[0], sep='')
+            for part in self.parts[1:-1]:
+                output_part(part, self.sep)
+            output_part(self.parts[-1], self.last_sep)
+            text = ''.join(tmp)
+        if self.add_period:
+            text = add_period(text)
+        return text
