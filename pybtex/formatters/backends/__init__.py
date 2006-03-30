c4f4ba2bb64363558e80e369a6557f79035c2119
golovizin <>
2006-03-30 18:38:31 +0000

    moved generic code to BackendBase

--- pybtex/formatters/backends/__init__.py
+++ pybtex/formatters/backends/__init__.py
@@ -1,6 +1,27 @@
 import locale
+import codecs
+
 class BackendBase:
     def __init__(self, encoding = None):
         if encoding is None:
             encoding = locale.getpreferredencoding()
         self.encoding = encoding
+
+    def write_prologue(self, maxlen):
+        pass
+
+    def write_epilogue(self):
+        pass
+
+    def write_bibliography(self, entries, filename):
+        self.f = codecs.open(filename, "w", self.encoding)
+        self.output = self.f.write
+        #FIXME: determine label width proprely
+        maxlen = max([len(e.label) for e in entries])
+
+        self.write_prologue(maxlen)
+        for entry in entries:
+            self.write_item(entry)
+        self.write_epilogue()
+
+        self.f.close()
--- pybtex/formatters/backends/latex.py
+++ pybtex/formatters/backends/latex.py
@@ -1,7 +1,6 @@
 from pybtex import utils
 from pybtex.richtext import Tag
 from pybtex.formatters.backends import BackendBase
-import codecs
 
 ndash = '--'
 
@@ -14,17 +13,12 @@ class Writer(BackendBase):
     def format_tag(self, tag_name, text):
         return r'\%s{%s}' % (tag_name, text)
     
+    def write_prologue(self, maxlen):
+        self.output('\\begin{thebibliography}{%s}' % ('8' * maxlen))
+
+    def write_epilogue(self):
+        self.output('\n\\end{thebibliography}\n')
+
     def write_item(self, entry):
         self.output('\n\n\\bibitem[%s]{%s}\n' % (entry.label, entry.key))
         self.output(entry.text.render(self))
-
-    def write_bibliography(self, entries, filename):
-        self.f = codecs.open(filename, "w", self.encoding)
-        self.output = self.f.write
-        maxlen = max([len(e.label) for e in entries])
-        #FIXME: determine label width proprely
-        self.output('\\begin{thebibliography}{%s}' % ('8' * maxlen))
-        for entry in entries:
-            self.write_item(entry)
-        self.output('\n\\end{thebibliography}\n')
-        self.f.close()
