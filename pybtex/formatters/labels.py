6b029c1118b29bf238069cac310b5251d9d64b6d
golovizin <>
2006-04-02 14:30:54 +0000

    removed author_year label style (it was broken anyway)

--- pybtex/formatters/labels.py
+++ pybtex/formatters/labels.py
@@ -17,8 +17,5 @@
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 # USA
 
-def author_year(e):
-    return e.authors[0].format('ll')[:3] + e['year'][-2:]
-
 def number(e):
     return str(e.number)
