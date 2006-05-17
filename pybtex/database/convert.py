a1a8e5124ff920cc3c3dd3d10fc7dd9c1516296b
golovizin <>
2006-05-17 10:54:47 +0000

    fixed errors in GPL permission statement

--- pybtex/database/convert.py
+++ pybtex/database/convert.py
@@ -13,8 +13,8 @@
 # General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with rdiff-backup; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
+# along with pybtex; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
 # USA
 
 """convert bibliography database from one format to another
