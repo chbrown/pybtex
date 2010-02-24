00c8c165e3f9a136a8950ca1fb0f2d9ade6731d6
Andrey Golovizin <golovizin@gmail.com>
2010-02-24 19:20:40 +0300

    Add a regression test for whitespace normalization in the BibTeX parser.

---
+++ pybtex/tests/bibtex_parser_test.py
@@ -0,0 +1,32 @@
+from pybtex.database import BibliographyData
+from pybtex.core import Entry
+from pybtex.database.input.bibtex import Parser
+from cStringIO import StringIO
+
+test_data = [
+    (
+        '''
+        ''',
+        BibliographyData(),
+    ),
+    (
+        '''@ARTICLE{
+                test,
+                title={Polluted
+                    with {DDT}.
+            },
+        }''',
+        BibliographyData({u'test': Entry('article', {u'title': 'Polluted with {DDT}.'})}),
+    ),
+]
+
+
+def _test(bibtex_input, correct_result):
+    parser = Parser(encoding='UTF-8')
+    parser.parse_stream(StringIO(bibtex_input))
+    result = parser.data
+    assert result == correct_result
+
+def test_bibtex_parser():
+    for bibtex_input, correct_result in test_data:
+        _test(bibtex_input, correct_result)
