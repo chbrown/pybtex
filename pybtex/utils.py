a647008eac4abb51798cc62c519c85861084cd05
Andrey Golovizin <golovizin@gmail.com>
2012-02-06 22:15:34 +0400

    Add pybtex.utils.CaseInsensitivDict.

--- pybtex/utils.py
+++ pybtex/utils.py
@@ -34,3 +34,107 @@ def memoize(f):
             memory[args] = f(*args)
         return memory[args]
     return new_f
+
+
+class CaseInsensitiveDict(dict):
+    """A dict with case-insensitive lookup.
+
+    >>> d = CaseInsensitiveDict(TesT='passed')
+    >>> d
+    CaseInsensitiveDict({'TesT': 'passed'})
+    >>> print d['TesT']
+    passed
+    >>> print d['test']
+    passed
+    >>> print d['Test']
+    passed
+    >>> d['Test'] = 'passed again'
+    >>> print d['test']
+    passed again
+    >>> 'test' in d
+    True
+    >>> print d.keys()
+    ['Test']
+    >>> for key in d:
+    ...     print key
+    Test
+    >>> for key, value in d.iteritems():
+    ...     print key, value
+    Test passed again
+    >>> len(d)
+    1
+    >>> del d['test']
+    >>> len(d)
+    0
+    >>> 'test' in d
+    False
+    >>> 'Test' in d
+    False
+
+    """
+    def __init__(self, *args, **kwargs):
+        initial_data = dict(*args, **kwargs)
+        super(CaseInsensitiveDict, self).__init__(*args, **kwargs)
+        self._keys = dict((key.lower(), key) for key in self)
+
+    def __setitem__(self, key, value):
+        """To implement lowercase keys."""
+        key_lower = key.lower()
+        try:
+            existing_key = self._keys[key_lower]
+        except KeyError:
+            pass
+        else:
+            super(CaseInsensitiveDict, self).__delitem__(existing_key)
+        super(CaseInsensitiveDict, self).__setitem__(key, value)
+        self._keys[key_lower] = key
+
+    def __getitem__(self, key):
+        existing_key = self._keys[key.lower()]
+        return super(CaseInsensitiveDict, self).__getitem__(existing_key)
+
+    def __delitem__(self, key):
+        key_lower = key.lower()
+        existing_key = self._keys[key_lower]
+        super(CaseInsensitiveDict, self).__delitem__(existing_key)
+        del self._keys[key_lower]
+
+    def pop(self, key, default=None):
+        raise NotImplementedError
+    
+    def popitem(self):
+        raise NotImplementedError
+    
+    def has_key(self, key):
+        raise NotImplementedError
+        
+    def __contains__(self, key):
+        return key.lower() in self._keys
+
+    def setdefault(self, key, default=None):
+        raise NotImplementedError
+    
+    def get(self, item, default=None):
+        """A case insensitive get."""
+        try:
+            return self[self._keys[key.lower()]]
+        except KeyError:
+            return default
+
+    def update(self, *args, **kwargs):
+        raise NotImplementedError
+
+    def copy(self):
+        """Create a new caselessDict object that is a copy of this one."""
+        return CaseInsensitiveDict(self)
+
+    def clear(self):
+        """Clear this caselessDict."""
+        super(CaseInsensitiveDict, self).clear()
+        self._keydict = {}
+
+    def __repr__(self):
+        """A caselessDict version of __repr__ """
+        return '{0}({1})'.format(
+            type(self).__name__, super(CaseInsensitiveDict, self).__repr__()
+        )
