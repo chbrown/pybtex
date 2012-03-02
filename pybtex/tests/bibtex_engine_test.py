bb8014fb300100aa8a3a027c39fabc41e82a7d7f
Andrey Golovizin <golovizin@gmail.com>
2012-03-02 22:48:10 +0400

    Move test data to pybtex.tests.data.

--- pybtex/tests/bibtex_engine_test.py
+++ pybtex/tests/bibtex_engine_test.py
@@ -29,22 +29,33 @@ def copy_resource(package, resource):
         data_file.write(data)
 
 
-def copy_files(test_name):
-    for filename in 'test.bib', 'test.aux':
-        copy_resource('pybtex.tests.bibtex_engine_test', posixpath.join(test_name, filename))
+def copy_files(bib_name, bst_name):
+    copy_resource('pybtex.tests.data', bib_name + '.bib')
+    copy_resource('pybtex.tests.data', bst_name + '.bst')
 
 
-def check_make_bibliography(test_name):
+def write_aux(aux_name, bib_name, bst_name):
+    with open(aux_name, 'wb') as aux_file:
+        aux_file.write('\\citation{*}\n')
+        aux_file.write('\\bibstyle{{{0}}}\n'.format(bst_name))
+        aux_file.write('\\bibdata{{{0}}}\n'.format(bib_name))
+
+def check_make_bibliography(bib_name, bst_name):
     with cd_tempdir() as tempdir:
-        copy_files(test_name)
+        copy_files(bib_name, bst_name)
+        write_aux('test.aux', bib_name, bst_name)
         with errors.capture() as stderr:  # FIXME check error messages
             bibtex.make_bibliography('test.aux')
         with open('test.bbl', 'rb') as result_file:
             result = result_file.read()
-        correct_result = pkgutil.get_data('pybtex.tests.bibtex_engine_test', posixpath.join(test_name, 'result.bbl'))
+        correct_result_name = '{0}_{1}.bbl'.format(bib_name, bst_name)
+        correct_result = pkgutil.get_data('pybtex.tests.data', correct_result_name)
         assert result == correct_result
 
 
 def test_bibtex_engine():
-    for test_name in 'xampl', 'cyrillic':
-        yield check_make_bibliography, test_name
+    for bib_name, bst_name in [
+        ('xampl', 'unsrt'),
+        ('cyrillic', 'unsrt'),
+    ]:
+        yield check_make_bibliography, bib_name, bst_name
